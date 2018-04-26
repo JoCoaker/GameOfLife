6f5ca6610c34d7b332cca357dce963415ab31f7b
Luke <utilize.shop@gmail.com>
2018-04-26 09:06:13 +0200

    init commit

---
+++ src/ParentView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ParentView extends JFrame {
+
+    public ParentView () {
+        setTitle("Game Of Life");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
